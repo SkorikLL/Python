ac0006ac3a62d31b86666a36b9c7ce5b210ff46d
SkorikLL <skorik555web@gmail.com>
2023-03-11 09:36:03 +0700

    Add work 7

--- work7/2.py
+++ work7/2.py
@@ -4,6 +4,6 @@ def print_operation_table(operation, num_rows = 6, num_columns = 6):
         for y in range(1, num_columns + 1):
             num = operation(x, y)
             nums.append(num)
-        print(*[x for x in nums])
+        print([x for x in nums])
 
 print_operation_table(lambda x,y: x*y, 5, 5)
